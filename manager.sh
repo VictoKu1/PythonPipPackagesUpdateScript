7eb0fb5701a505c9b2574efc7c32eabb83ab2d2d
VictoKu1 <kushnir.victor@gmail.com>
2022-05-16 05:46:57 +0300

    .

--- manager.sh
+++ manager.sh
@@ -2,14 +2,11 @@
 
 
 
-if [ "$(id -u)" != "0" ]; then
+if [ "$(id -u)" != "0" ]; 
+then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
-
-
-
-
 
 
 clear
@@ -18,27 +15,22 @@ echo "1. Install"
 echo "2. Remove"
 read -p "Enter your choice [1-2]: " choice
 
-
-
-
-
-case $choice in
+case $choice 
+in
    1) echo "Installing....."
       touch /bin/updatePip
       chmod +x /bin/updatePip
       echo "#!/bin/sh" >> /bin/updatePip
       local_dir=$(pwd)
-      echo "" >> /bin/updatePip
-      echo "" >> /bin/updatePip
-      echo "" >> /bin/updatePip
-      echo "" >> /bin/updatePip
-      echo "" >> /bin/updatePip
+      for i in {1..5} 
+         do
+            echo "" >> /bin/updatePip
+         done
       echo "sh $local_dir/update.sh" >> /bin/updatePip;;
    2) echo "Removing....."
       rm -rf /bin/updatePip requirements.txt ;;
    *) echo "Invalid choice";;
 esac
-
 
 
 
