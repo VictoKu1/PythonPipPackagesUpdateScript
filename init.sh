53771ae75301e04a450c44b8a9b369f0a0115486
VictoKu1 <kushnir.victor@gmail.com>
2022-05-16 05:31:16 +0300

    .

--- init.sh
+++ init.sh
@@ -8,18 +8,32 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 
-touch /bin/updatePip
 
 
 
-chmod +x /bin/updatePip
+
+# Ask the user if he wants to install or remove the service
+echo "Do you want to install or remove the service?"
+echo "1. Install"
+echo "2. Remove"
+read -p "Enter your choice [1-2]: " choice
+# Make sure the choice is valid
 
 
-echo "#!/bin/sh" >> /bin/updatePip
-local_dir=$(pwd)
-echo "" >> /bin/updatePip
-echo "" >> /bin/updatePip
-echo "" >> /bin/updatePip
-echo "" >> /bin/updatePip
-echo "" >> /bin/updatePip
-echo "sh $local_dir/update.sh" >> /bin/updatePip
+
+case $choice in
+   1) echo "You chose to install"
+      touch /bin/updatePip
+      chmod +x /bin/updatePip
+      echo "#!/bin/sh" >> /bin/updatePip
+      local_dir=$(pwd)
+      echo "" >> /bin/updatePip
+      echo "" >> /bin/updatePip
+      echo "" >> /bin/updatePip
+      echo "" >> /bin/updatePip
+      echo "" >> /bin/updatePip
+      echo "sh $local_dir/update.sh" >> /bin/updatePip;;
+   2) echo "You chose to remove"
+      rm -rf /bin/updatePip ../$(basename $0);;
+   *) echo "Invalid choice";;
+esac
